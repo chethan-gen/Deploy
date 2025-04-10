86d19436c67c2c8b6f657a7cc01b1a45237a0dde
chethan-gen <chaithuregala123@gmailcom>
2025-04-10 11:26:24 +0530

    adding files

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1>REGALA VENKATA LAKSHMI CHETHAN</h1>
+     <h2>Squad 89</h2>
+     <h2>chaithuregala123@gmail.com</h2>
+     <h2>https://github.com/chethan-gen</h2>
+    </>
+  )
+}
+
+export default App
